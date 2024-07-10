008885f562aea70c7be18af3477063a8567fb087
sethjameson <seth.jameson11@gmail.com>
2024-07-10 16:25:40 -0400

    Add files via upload

---
+++ src/main/java/com/example/afinal/plank.java
@@ -0,0 +1,36 @@
+package com.example.afinal;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class plank extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_plank);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button one = findViewById(R.id.tips5);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(plank.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

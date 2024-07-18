49b18fe037550cb92f5c120a8f33074d40335c2f
sethjameson <seth.jameson11@gmail.com>
2024-07-18 11:54:16 -0400

    Add files via upload

---
+++ app/src/main/java/com/zybooks/engproject/highknee.java
@@ -0,0 +1,36 @@
+package com.example.afinal;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class highknee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_highknee);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button one = findViewById(R.id.tips);
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(highknee.this, Tips_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
